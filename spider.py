92e6640e47cda0595b24e509bc5b2ce7c815a987
isaacgr <isaacgrowell@gmail.com>
2018-11-04 11:13:34 -0500

    Add methods to get links, update queue and update files

--- spider.py
+++ spider.py
@@ -26,7 +26,8 @@ class Spider:
     def boot():
         create_project_dir(Spider.project_name)
         create_data_files(Spider.project_name, Spider.base_url)
-        Spider.queue = file_to_set(Spider.crawled_file)
+        Spider.queue = file_to_set(Spider.queue_file)
+        Spider.crawled = file_to_set(Spider.crawled_file)
 
     @staticmethod
     def crawl_page(thread_name, page_url):
@@ -37,4 +38,35 @@ class Spider:
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
             Spider.update_files()
+
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            response = urlopen(page_url)
+            if 'text/html' in response.getheader('Content-Type'):
+                html_bytes = response.read()
+                html_string = html_bytes.decode('utf-8')
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+        except:
+            print('Error: can not crawl page')
+            return set()
+        return finder.page_links()
+
+    @staticmethod
+    def add_links_to_queue(links):
+        for url in links:
+            if url in Spider.queue:
+                continue
+            if url in Spider.crawled:
+                continue
+            if Spider.domain_name not in url:
+                continue
+            Spider.queue.add(url)
+
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        set_to_file(Spider.crawled, Spider.crawled_file)
 
