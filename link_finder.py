a678030b2b9c05c5e3a9eb11c34bdb40405ef0b0
isaacgr <isaacgrowell@gmail.com>
2018-11-03 23:24:07 -0400

    Fix whitespace Add comments

--- link_finder.py
+++ link_finder.py
@@ -2,25 +2,25 @@ from html.parser import HTMLParser
 from urllib import parse
 
 class LinkFinder(HTMLParser):
-	
-	def __init__(self, base_url, page_url):
-		super().__init__()
-		self.base_url = base_url
-		self.page_url = page_url
-		self.links = set()
 
+    def __init__(self, base_url, page_url):
+        super().__init__()
+        self.base_url = base_url
+        self.page_url = page_url
+        self.links = set()
 
-	def handle_starttag(self, tag, attrs):
-		if tag == 'a':
-			for (attribute, value) in attrs:
-				if attribute == 'href':
-					url = parse.urljoin(self.base_url, value)
-					self.links.add(url)
-					
-	
-	def page_links(self):
-		return self.links
+        # get all the start tags of the html
+        def handle_starttag(self, tag, attrs):
+            if tag == 'a':  # if the start tag is a link, get the href url
+                for (attribute, value) in attrs:
+                    if attribute == 'href':
+                        url = parse.urljoin(self.base_url, value)
+                        self.links.add(url)
 
+        # return the list of links
+        def page_links(self):
+            return self.links
 
-	def error(self, message):
-		pass
+        # handle errors
+        def error(self, message):
+            pass
