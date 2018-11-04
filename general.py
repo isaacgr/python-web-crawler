619083f858f3ac8051595d67131b0949f7f67fae
isaacgr <isaacgrowell@gmail.com>
2018-11-03 23:23:33 -0400

    Remove whitespace Add comments

--- general.py
+++ general.py
@@ -7,44 +7,44 @@ import os
 
 # create directory for website if it doesnt already exist
 def create_project_dir(directory):
-	if not os.path.exists(directory):
-		print('Creating project ' + directory)
-		os.makedirs(directory)
+    if not os.path.exists(directory):
+        print('Creating project ' + directory)
+        os.makedirs(directory)
 
 # create queue and crawled files if not already created
 def create_data_files(project_name, base_url):
-	queue = project_name + '/queue.txt'
-	crawled = project_name +'/crawled.txt'
-	if not os.path.isFile(queue):
-		write_file(queue, base_url)
-	if not os.path.isFile(crawled):
-		write_file(crawled, '')
+    queue = project_name + '/queue.txt'
+    crawled = project_name +'/crawled.txt'
+    if not os.path.isFile(queue):
+        write_file(queue, base_url)
+    if not os.path.isFile(crawled):
+        _file(crawled, '')
 
 # create a new file
 def write_file(path, file_name):
-	f = open(path, 'w')
-	f.write(data)
-	f.close()
+    f = open(path, 'w')
+    f.write(data)
+    f.close()
 
 # add data onto an existing file
 def append_to_file(path, data):
-	with open(path, 'a') as file:
-		file.write(data+'\n')
+    with open(path, 'a') as file:
+        file.write(data+'\n')
 
 # delete the contents of a file
 def delete_file_contents(path):
-	with open(path, 'w'):
-		pass	# do nothing, will write empty file
+    with open(path, 'w'):
+        pass	# do nothing, will write empty file
 
 # read a file and convert each line to set items
 def file_to_set(file_name):
-	results = set()
-	with open(file_name, 'rt') as f:
-		for line in f:
-			results.add(line.replace('\n', ''))
+    results = set()
+    with open(file_name, 'rt') as f:
+        for line in f:
+            results.add(line.replace('\n', ''))
 
 # iterate through a set, each item will be a new line in the file
 def set_to_file(links, file):
-	delete_file_contents(file)
-	for link in sorted(links):
-		append_to_file(file, link)
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file, link)
